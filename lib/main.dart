4f24baeef4e434913d05b33767cc7e79f853cd5f
unknown <manujales10@gmail.com>
2020-02-27 11:40:31 -0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart' ;
+void main() => runApp(
+  MaterialApp(
+    home: Center(
+      child: Container(
+        color: Colors.blue[100],
+        child: Text("Manuella",
+            style: TextStyle(color: Colors.black)
+        ),
+      ),
+    ),
+  ),
+
+);
+
+
+
